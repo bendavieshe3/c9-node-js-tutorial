2dd81d6b3b369ccfb26db55d95eb843a57fdecae
Ben Davies <bendavies@he3.com.au>
2013-01-20 00:11:34 -0500

    removing non-express files

--- helloworld-http.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello World\n');
-    console.log(req.headers['x-forwarded-for']);
-}).listen(process.env.PORT, process.env.IP);
